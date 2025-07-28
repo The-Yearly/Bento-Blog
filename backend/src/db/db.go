496ad6d4216f08814fce1c6b1e0549309a862657
The-Yearly <theyearlysclub@gmail.com>
2025-07-29 04:52:36 +0530

    Changes For Hosting

--- backend/src/db/db.go
+++ backend/src/db/db.go
@@ -17,7 +17,13 @@ func ConnectToDb() {
 	if err != nil {
 		log.Fatal("Failed To Find Env")
 	}
-	dsn := fmt.Sprintf("host=%s user=%s dbname=%s port=5432 sslmode=disable", os.Getenv("db_host"), os.Getenv("user"), os.Getenv("dbname"))
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"))
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed To Connect To Db")
